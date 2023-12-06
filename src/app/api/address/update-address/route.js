9125292a34cba9c8e2ade792be39cd119b0b7870
Nicholas <nickndolo95@gmail.com>
2023-12-06 17:31:04 +0300

    creat updat-address api route

---
+++ src/app/api/address/update-address/route.js
@@ -0,0 +1,50 @@
+
+import { NextResponse } from 'next/server';
+import { connectToDB } from '@/database';
+import AuthUser from '@/middleware/AuthUser';
+import Address from '@/models/address';
+
+export const dynamic = "force-dynamic";
+
+export async function PUT(req){
+  try {
+    await connectToDB()
+
+    const isAuthUser = AuthUser(req)
+    if(isAuthUser) {
+      const data = await req.json();
+    
+    const { _id, fullName, city, address, county, postalCode } = data
+    
+    const updatedAddress = Address.findOneAndUpdate({
+      _id: _id
+    }, {fullName, city, address, county, postalCode}, {new: true})
+
+    if(updatedAddress) {
+      return NextResponse.json({
+        success: true,
+        message: "Address updated successfully"
+      })
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Failed to update address ! Please try again"
+      })
+    }
+
+    } else{
+      return NextResponse.json({
+        success: false,
+        message: "You are not authenticated"
+      })
+    }
+    
+
+  } catch (e) {
+    console.log(e)
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong ! Please try again later"
+    })
+  }
+}
