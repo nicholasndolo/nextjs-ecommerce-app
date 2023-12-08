f6108dc10e3d44d531d3aa27f5bbed5f23f7fb7a
Nicholas <nickndolo95@gmail.com>
2023-12-08 19:07:37 +0300

    create stripe service

--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -1,6 +1,7 @@
 
 import { NextResponse } from 'next/server';
 import Checkout from '../../checkout/page';
+import AuthUser from '@/middleware/AuthUser';
 
 const stripe = require('stripe')('paste-stripe-secret-key-here');
 
@@ -8,20 +9,29 @@ export const dynamic = "force-dynamic"
 
 export async function POST(req){
   try{
-    const res = await res.json()
+    const isAuthUser = await AuthUser(req)
+    if(isAuthUser){
+      const res = await res.json()
 
-    const session = await stripe.Checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items: res,
-      mode: 'payment',
-      success_url: 'http://localhost:300/checkout' + '?status=success',
-      cancel_url: 'http://localhost:300/checkout' + '?status=cancel'
-    })
+      const session = await stripe.Checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: res,
+        mode: 'payment',
+        success_url: 'http://localhost:300/checkout' + '?status=success',
+        cancel_url: 'http://localhost:300/checkout' + '?status=cancel'
+      })
+  
+      return NextResponse.json({
+        success: true,
+        id: session.id
+      })
 
-    return NextResponse({
-      success: true,
-      id: session.id
-    })
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: 'You are not authenticated'
+      })
+    }
 
   } catch(e){
     console.log(e)
